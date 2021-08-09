fcc56d5bf8ee24b11339ab226a9db7b5dc018581
Nicholas <nicholasoliver@peppersquare.com>
2021-08-09 18:31:25 +0530

    added sliders for gallery and services section only for mobile

--- js/custom_js.js
+++ js/custom_js.js
@@ -1,13 +1,41 @@
 
 $(document).ready(function(){
   $('#header').load("header.html");
-    $('#footer').load("footer.html");
+  $('#footer').load("footer.html");
   $('.single-item').slick({
     arrows: false,
-     dots: true,
-     autoplay: true,
-  autoplaySpeed: 2000
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000
   });
 
-    $('<span>Amazing responsive carousel</span>').prependTo('.fr-div .slick-dots');
+  $('<span>Amazing responsive carousel</span>').prependTo('.fr-div .slick-dots');
+
+  $('.sliding-item').slick({
+    arrows: false,
+    dots: false,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplaySpeed: 2000,
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true
+      }
+    }
+  ]
+  });
 });
