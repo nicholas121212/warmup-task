4dcca8d3bd3a1b4b727c68ad121d017f790bc83c
Nicholas <nicholasoliver@peppersquare.com>
2021-08-10 16:53:39 +0530

    corrections made as per feedback

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
 const fileinclude = require('gulp-file-include');
 const browserSync = require('browser-sync').create();
+const minify = require('gulp-minify');
 
 function defaultTask(cb) {
   // place code for your default task here
@@ -10,57 +11,11 @@ function defaultTask(cb) {
 }
 
 
-const paths = {
-  scripts: {
-    src: './',
-    dest: './build/'
-  }
-};
-
 // Reload Server
 async function reload() {
   server.reload();
 }
 
-// Copy assets after build
-async function copyAssets() {
-  gulp.src(['assets/**/*'])
-    .pipe(gulp.dest(paths.scripts.dest));
-}
-
-// Build files html and reload server
-async function buildAndReload() {
-  await includeHTML();
-  await copyAssets();
-  reload();
-}
-
-function includeHTML(){
-  return gulp.src([
-    '*.html',
-    '!header.html', // ignore
-    '!footer.html' // ignore
-    ])
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(gulp.dest(paths.scripts.dest));
-}
-exports.includeHTML = includeHTML;
-
-// exports.default = async function() {
-//   // Init serve files from the build folder
-//   server.init({
-//     server: {
-//       baseDir: paths.scripts.dest
-//     }
-//   });
-//   // Build and reload at the first time
-//   buildAndReload();
-//   // Watch task
-//   watch(["*.html","assets/**/*"], series(buildAndReload));
-// };
 
 
 
@@ -85,4 +40,12 @@ function watch(){
 
 exports.style = style;
 exports.watch = watch;
-exports.default = defaultTask
+exports.default = defaultTask;
+
+
+
+gulp.task('compress', function() {
+  gulp.src(['js/*.js', 'dist/*.css'])
+    .pipe(minify())
+    .pipe(gulp.dest('dist'))
+});
