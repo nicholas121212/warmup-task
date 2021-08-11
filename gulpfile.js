c00dddcc0053ee660d73624a01651ded45d0dfed
Nicholas <nicholasoliver@peppersquare.com>
2021-08-11 15:05:14 +0530

    Added functions for minification of Js and css files and impacted on watch. removed build/index.html as the file was not needde

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,20 @@ var sass = require('gulp-sass')(require('sass'));
 const fileinclude = require('gulp-file-include');
 const browserSync = require('browser-sync').create();
 const minify = require('gulp-minify');
+var uglify = require('gulp-uglify');
+var pipeline = require('readable-stream').pipeline;
+const minifyCss = require('gulp-clean-css');
 
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+
+
+function defaultTask(cb) {cb();
 }
-
-
-// Reload Server
-async function reload() {
-  server.reload();
-}
-
-
-
-
 function style(){
   return gulp.src('./sass/*.scss')
   .pipe(sass())
   .pipe(gulp.dest('./dist'))
   .pipe(browserSync.stream());
 }
-
-
-
-
 function watch(){
  browserSync.init({
   server:{ baseDir:'./'}
@@ -36,16 +25,26 @@ function watch(){
  gulp.watch('./sass/*.scss', style);
  gulp.watch('./*.html').on('change', browserSync.reload);
  gulp.watch('./js/*.js').on('change', browserSync.reload);
+ gulp.watch('./dist/*.css',bundleCss);
+  gulp.watch('./js/*.js',compress);
 }
 
 exports.style = style;
 exports.watch = watch;
 exports.default = defaultTask;
 
+const compress = () => {
+   return gulp.src('./js/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./dist/minifiedJs/'));
+}
 
+exports.compress = compress;
 
-gulp.task('compress', function() {
-  gulp.src(['js/*.js', 'dist/*.css'])
-    .pipe(minify())
-    .pipe(gulp.dest('dist'))
-});
+const bundleCss = () => {
+  return gulp.src('./dist/*.css')
+  .pipe(minifyCss())
+  .pipe(gulp.dest('./dist/minifiedCss/'));
+}
+
+exports.bundleCss = bundleCss;
